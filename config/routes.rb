8b421cb9229a0614709748136bf82ac361d5660a
Manikandan TS <manikandan.citeee@gmail.com>
2018-10-23 14:46:39 -0700

    Routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'home#activations'
 
   post '/api/configuration', to: 'configuration#get'
+  get '/api/activations', to: 'activations#get'
+  post '/api/activations', to: 'activations#create'
+  put '/api/activations', to: 'activations#update'
+  get '/api/devices', to: 'devices#get'
 
   resources :gateways, only: [:index]
   resources :ts_names, only: [:index]
